113b6a2c79d854ab9df6d8beb87673be95d9ae53
Wocked61 <dylanphan88@gmail.com>
2025-10-29 10:32:09 -0700

    made the basic header and footer

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>Group</p>
+      <p>Members: Dylan Phan, Vincent Nguyen, Matt Lim, Helen Ngo</p>
+      
+    </footer>
+  )
+}
---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+
+
+function Header() {
+  return (
+    <header>
+      <h1>Capstone Project : Procuratio</h1>
+      <nav>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
