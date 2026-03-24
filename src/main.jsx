f816c6468271567e2664dd07712959b899eba355
Wocked61 <dylanphan88@gmail.com>
2026-03-24 09:54:09 -0700

    made improvements to the calendar page allowing click and plan feature

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.jsx'
 import Login from './Login.jsx'
 import Signup from './Signup.jsx'
@@ -33,8 +34,12 @@ function RootApp() {
   )
 }
 
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RootApp />
+    <GoogleOAuthProvider clientId={googleClientId}>
+      <RootApp />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
